b48fe82a4eddd65851eea03a0329ac1fee09bc3a
Emily Nozaki <emiri3128.gmail.com>
2022-02-14 14:08:00 +0900

    Initial Commit

---
+++ miffy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miffy
+//
+//  Created by Emily Nozaki on 2022/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
