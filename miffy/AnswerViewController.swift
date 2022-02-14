5d1fa10ee762833554b7d64d9a99f7bd8f5f6298
Emily Nozaki <emiri3128.gmail.com>
2022-02-14 16:10:47 +0900

    できた〜

---
+++ miffy/AnswerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AnswerViewController.swift
+//  miffy
+//
+//  Created by Emily Nozaki on 2022/02/14.
+//
+
+import UIKit
+
+class AnswerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ miffy/GuessMiffyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GuessMiffyViewController.swift
+//  miffy
+//
+//  Created by Emily Nozaki on 2022/02/14.
+//
+
+import UIKit
+
+class GuessMiffyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
